e0cfcb16d8c5c2d8a761368bda97b2822ec59cc0
Jeffry Molanus <jeffry.molanus@gmail.com>
2020-06-19 09:46:17 +0200

    thread: comment out poller timeouts
    The poller timeout flood the logs making it hard
    to use -L all

--- lib/thread/thread.c
+++ lib/thread/thread.c
@@ -606,11 +606,11 @@ thread_poll(struct spdk_thread *thread, uint32_t max_msgs, uint64_t now)
 			poller->busy_count++;
 		}
 
-#ifdef DEBUG
-		if (poller_rc == -1) {
-			SPDK_DEBUGLOG(SPDK_LOG_THREAD, "Poller %s returned -1\n", poller->name);
-		}
-#endif
+//#ifdef DEBUG
+//		if (poller_rc == -1) {
+//			SPDK_DEBUGLOG(SPDK_LOG_THREAD, "Poller %s returned -1\n", poller->name);
+//		}
+//#endif
 
 		if (poller->state == SPDK_POLLER_STATE_UNREGISTERED) {
 			TAILQ_REMOVE(&thread->active_pollers, poller, tailq);
@@ -650,11 +650,11 @@ thread_poll(struct spdk_thread *thread, uint32_t max_msgs, uint64_t now)
 			poller->busy_count++;
 		}
 
-#ifdef DEBUG
-		if (timer_rc == -1) {
-			SPDK_DEBUGLOG(SPDK_LOG_THREAD, "Timed poller %s returned -1\n", poller->name);
-		}
-#endif
+//#ifdef DEBUG
+//		if (timer_rc == -1) {
+//			SPDK_DEBUGLOG(SPDK_LOG_THREAD, "Timed poller %s returned -1\n", poller->name);
+//		}
+//#endif
 
 		if (poller->state == SPDK_POLLER_STATE_UNREGISTERED) {
 			TAILQ_REMOVE(&thread->timed_pollers, poller, tailq);
