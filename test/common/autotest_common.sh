b631bfecf0d34da9c71631ef00eba6fd296e211a
shubham <shubham.bajpai@mayadata.io>
2021-12-02 20:22:13 +0530

    Fixing .sh lint
    Signed-off-by: shubham <shubham.bajpai@mayadata.io>

--- test/common/autotest_common.sh
+++ test/common/autotest_common.sh
@@ -671,7 +671,7 @@ function process_core() {
 		cat <<- BT
 			##### CORE BT ${core##*/} #####
 
-			$(<"$core")
+			$(< "$core")
 
 			--
 		BT
@@ -1406,7 +1406,7 @@ function pap() {
 	while read -r file; do
 		cat <<- FILE
 			--- $file ---
-			$(<"$file")
+			$(< "$file")
 			--- $file ---
 		FILE
 		rm -f "$file"
